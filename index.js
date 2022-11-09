f0b0788a536f0fd49f3317dd8c76593e4cedd1c5
krupal01 <bhuvakrupal01@gmail.com>
2022-11-09 17:54:11 +0530

    html file derectly , without extension in query

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ const app = express();
 const path = require('path');
 
 const publicPath = path.join(__dirname,"public_page")
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
+
+app.get("",(req,resp)=>{
+    resp.sendFile(`${publicPath}/index.html`)
+})
+
+app.get("/about",(req,resp)=>{
+    resp.sendFile(`${publicPath}/about.html`)
+})
 app.listen(5000)
