8db310813835b3061bcfbcae6e17518de24dc8c0
krupal01 <bhuvakrupal01@gmail.com>
2022-11-09 09:07:11 +0530

    create file

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const app = require('./app')
-console.log(app.xyz())
-
-let arr = [4,5,2,6,8,54,4,2];
-const result = arr.filter((item)=>{
-    return item>=5
-});
-console.warn(result);
+const fs = require('fs');
+console.log(">>",__filename)
+fs.writeFileSync("helloWorld.txt","hello world")
