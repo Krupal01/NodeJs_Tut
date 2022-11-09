03e092de24dc76d6630f6fc2df6aba54528be025
krupal01 <bhuvakrupal01@gmail.com>
2022-11-09 16:52:51 +0530

    promise

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 console.log("first log")
 
-setTimeout(()=>{
-    console.log("second log")
-},2000)
+let data = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve(30)
+    },2000)
+})
 
+data.then((data)=>{
+    console.log(data)
+})
 console.log("third log")
