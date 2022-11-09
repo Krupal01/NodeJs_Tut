eb47319eb25fb317323e524f9460d10349f4ecb4
krupal01 <bhuvakrupal01@gmail.com>
2022-11-09 09:15:56 +0530

    delete

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 console.log(">>",__filename)
 // fs.writeFileSync("helloWorld.txt","hello world")
-fs.appendFileSync("helloWorld.txt"," , hello world")
-fs.readFile("helloWorld.txt",function(err,data){
-    console.log(data);
-})
-fs.readFile("helloWorld.txt","utf8",function(err,data){
-    console.log(data);
-})
+// fs.appendFileSync("helloWorld.txt"," , hello world")
+// fs.readFile("helloWorld.txt",function(err,data){
+//     console.log(data);
+// })
+// fs.readFile("helloWorld.txt","utf8",function(err,data){
+//     console.log(data);
+// })
+
+fs.unlinkSync("./helloWorld.txt")
