5ad79bf8f8c00fb2a57c30c79ee8a164dfd8cac5
krupal01 <bhuvakrupal01@gmail.com>
2022-11-09 09:49:39 +0530

    console input

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const fs = require('fs');
-
-console.log(">>",__filename)
-fs.writeFileSync("helloWorld.txt","hello world")
-fs.appendFileSync("helloWorld.txt"," , hello world")
-fs.readFile("helloWorld.txt",function(err,data){
-    console.log(data);
-})
-fs.readFile("helloWorld.txt","utf8",function(err,data){
-    console.log(data);
-})
-
-fs.unlinkSync("./helloWorld.txt")
+const input = process.argv;
+console.log(input)
+console.log(input[2]*input[2])
