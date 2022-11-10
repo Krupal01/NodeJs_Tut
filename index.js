27abb530ffe8db6ed05318cc1a807443587a48ec
krupal01 <bhuvakrupal01@gmail.com>
2022-11-10 09:57:29 +0530

    route middle ware

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const reqFilter = require('./middle_ware')
 
 const publicPath = path.join(__dirname,"public_page")
 // app.use(express.static(publicPath));
 
-const reqFilter = (req , resp , next)=>{
-    if(!req.query.age){
-        resp.send('please provide age')
-    }
-    else if(req.query.age < 18){
-        resp.send('you cannot access this site')
-    }
-    else {
-        next()
-    }
-}
 
-app.use(reqFilter)
+// app.use(reqFilter)
 
 app.get("",(req,resp)=>{
     resp.send("index page")
 })
 
-app.get("/user",(req,resp)=>{
+app.get("/user",reqFilter,(req,resp)=>{
     resp.send("user page")
 })
 
