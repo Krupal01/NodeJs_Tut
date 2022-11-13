433f3ed91a3e39a5b5dc56b5a6fac3010730951c
krupal01 <bhuvakrupal01@gmail.com>
2022-11-13 13:51:53 +0530

    insert data

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const dbConnection = require('./mongodb')
 
-async function main(){
-    const data = await (await dbConnection()).find({}).toArray()
-    console.log(data)
+// async function main(){
+//     const data = await (await dbConnection()).find({}).toArray()
+//     console.log(data)
+// }
+
+// main()
+
+const insert = async () =>{
+    const db = await dbConnection()
+    const result = await db.insertOne({
+        name:"oppo",
+        price:520,
+        model:"a35"
+    })
+    console.log(result)
+
+    if (result.acknowledged){
+        console.log("data inserted")
+    }
 }
 
-main()
+insert()
