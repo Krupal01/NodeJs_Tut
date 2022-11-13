6bcdfc1e3aabb72ea22fa7d3018a13ff6dbbdd67
krupal01 <bhuvakrupal01@gmail.com>
2022-11-13 14:00:09 +0530

    update delete

--- index.js
+++ index.js
@@ -7,18 +7,36 @@ const dbConnection = require('./mongodb')
 
 // main()
 
-const insert = async () =>{
+// const insert = async () =>{
+//     const db = await dbConnection()
+//     const result = await db.insertOne({
+//         name:"oppo",
+//         price:520,
+//         model:"a35"
+//     })
+//     console.log(result)
+
+//     if (result.acknowledged){
+//         console.log("data inserted")
+//     }
+// }
+
+// insert()
+
+// const update = async () =>{
+//     const db = await dbConnection()
+//     const result = await db.updateOne(
+//         {name:"oppo"},
+//         {$set: {name:'vivo',price:60}}
+//     )
+//     console.log(result)
+
+// }
+// update()
+
+const deleteData = async () =>{
     const db = await dbConnection()
-    const result = await db.insertOne({
-        name:"oppo",
-        price:520,
-        model:"a35"
-    })
+    const result = await db.deleteOne({name:'vivo'})
     console.log(result)
-
-    if (result.acknowledged){
-        console.log("data inserted")
-    }
 }
-
-insert()
+deleteData()
